934be02a0a5a871bdda8439891df5d79d2e317dd
Jenna Claudia Andriamanantena <jennaandria@gmail.com>
2024-08-28 17:34:48 +0200

    Step 1.3: Add valid_position? method to Board class and corresponding tests

--- lib/board.rb
+++ lib/board.rb
@@ -6,4 +6,26 @@ class Board
   def display
     @grid.each { |row| puts row.join(' ') }
   end
+
+  def place_ship(ship, x, y, orientation)
+    if valid_position?(ship, x, y, orientation)
+      ship.coordinates(x, y, orientation).each do |coord|
+        @grid[coord[0]][coord[1]] = 'S'
+      end
+    else
+      raise "Invalid position for the ship"
+    end
+  end
+
+  def valid_position?(ship, x, y, orientation)
+    ship.coordinates(x, y, orientation).all? do |coord|
+      within_bounds?(coord[0], coord[1]) && @grid[coord[0]][coord[1]] == '-'
+    end
+  end
+
+  private
+
+  def within_bounds?(x, y)
+    x.between?(0, 4) && y.between?(0, 4)
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,9 @@
 require 'board'
+require 'ship'
 
 RSpec.describe Board do
   let(:board) { Board.new }
+  let(:ship) { Ship.new(3) }
 
   describe '#initialize' do
     it 'initializes a 5x5 grid' do
@@ -13,6 +15,30 @@ RSpec.describe Board do
   describe '#display' do
     it 'displays the grid in the console' do
       expect { board.display }.to output.to_stdout
+    end
+  end
+
+  describe '#place_ship' do
+    it 'places a ship on the board' do
+      board.place_ship(ship, 0, 0, :horizontal)
+      expect(board.instance_variable_get(:@grid)[0][0]).to eq('S')
+    end
+  end
+
+  describe '#valid_position?' do
+    let(:ship) { Ship.new(3) }
+
+    it 'returns true for a valid position' do
+      expect(board.valid_position?(ship, 0, 0, :horizontal)).to be true
+    end
+
+    it 'returns false for an invalid position out of bounds' do
+      expect(board.valid_position?(ship, 4, 4, :horizontal)).to be false
+    end
+
+    it 'returns false if ship overlaps another ship' do
+      board.place_ship(ship, 0, 0, :horizontal)
+      expect(board.valid_position?(ship, 0, 0, :horizontal)).to be false
     end
   end
 end
